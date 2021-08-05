b35e157199260070f960aa5f2cf8f5deae302a38
Adir Avraham <adir@koch.local>
2021-08-05 18:48:39 +0300

    first-commit

---
+++ src/components/cases/index.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { cases } from '../../data/data';
+
+
+export default function Cases() {
+
+return (
+    <>
+    <h1 className="text-center py-5">רשימת תיקים</h1>
+    <div className="container">
+
+<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  הוספת תיק חדש
+</button>
+
+
+<div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog modal-dialog-centered">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">הוספת תיק חדש</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+      <div className="mb-3">
+  <label htmlFor="exampleFormControlInput1" className="form-label">שם תיק:</label>
+  <input type="text" className="form-control" id="exampleFormControlInput1"/>
+</div>
+<div className="mb-3">
+  <label htmlFor="exampleFormControlTextarea1" className="form-label">משימות:</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows={4}></textarea>
+</div>
+<div className="mb-3">
+  <label htmlFor="exampleFormControlInput1" className="form-label">מועד דיון:</label>
+  <input type="date" className="form-control" id="exampleFormControlInput1"/>
+</div>
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">בטל</button>
+        <button type="button" className="btn btn-primary">שמור</button>
+      </div>
+    </div>
+  </div>
+</div>
+        <table className="table table-bordered table-hover my-5">
+  <thead>
+    <tr>
+      <th scope="col">תיק</th>
+      <th scope="col">משימות</th>
+      <th scope="col">מועד דיון</th>
+      <th scope="col">סטטוס</th>
+    </tr>
+  </thead>
+  <tbody>
+  {cases.map((item: any) => (
+    <tr key={item.caseId}>
+    <th scope="row">{item.caseName}</th>
+    <td>{item.caseTasks}</td>
+    <td>{item.nextDate}</td>
+    <td>  
+      {item.status}
+      <div className="form-check form-switch">
+        {item.status ? 
+        <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked/> :
+        <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault"/> 
+
+        
+      }
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{item.status ? "פעיל" : "סגור"}</label>
+</div>
+    </td>
+  </tr>
+
+          ))}
+ 
+  </tbody>
+</table>
+    </div>
+</>
+)
+
+
+}
---
+++ src/data/data.tsx
@@ -0,0 +1,40 @@
+export const cases = [
+    {
+        caseId: 1,
+        caseName: "בנארי",
+        caseTasks: "כתב הגנה",
+        nextDate: new Date().toLocaleDateString("uk"),
+        status: true,
+
+    },
+    {
+        caseId: 2,
+        caseName: "שמשי",
+        caseTasks: "סיכומים",
+        nextDate: new Date().toLocaleDateString("uk"),
+        status: false,
+    },
+    {
+        caseId: 3,
+        caseName: "אלדד",
+        caseTasks: "האם הוגשה תגובה לבקשה",
+        nextDate: new Date().toLocaleDateString("uk"),
+        status: true,
+    },
+    {
+        caseId: 4,
+        caseName: "גרייבר",
+        caseTasks: "דיווח ראשון",
+        nextDate: new Date().toLocaleDateString("uk"),
+        status: false,
+    },
+    {
+        caseId: 5,
+        caseName: "ארד מהנדסים",
+        caseTasks: "להתקשר ללקוח",
+        nextDate: new Date().toLocaleDateString("uk"),
+        status: true,
+    },
+
+
+]
