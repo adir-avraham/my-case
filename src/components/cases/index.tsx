13e8c8124958023773ed9caba6d816fe2c83dd73
adir-avraham <adir.avraham@gmail.com>
2021-08-12 08:04:09 +0300

    progress

--- src/components/cases/index.tsx
+++ src/components/cases/index.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { cases } from '../../data/data';
 
 
 export default function Cases() {
+
+  const initialState = {
+    caseId: Math.random(),
+    caseName: "",
+    caseTasks: "",
+    nextDate: new Date().toLocaleDateString("uk"),
+    status: true,
+  }
+
+  const [caseName, setCaseName] = useState("");
+  const [caseTasks, setCaseTasks] = useState("");
+  const [cases1, setCases] = useState(cases);
+  function saveCase () {
+    cases.push({
+      caseId: Math.random(),
+      caseName: caseName,
+      caseTasks: caseTasks,
+      nextDate: new Date().toLocaleDateString("uk"),
+      status: true,
+    })
+    setCases(cases)
+  }
 
 return (
     <>
@@ -24,11 +46,11 @@ return (
       <div className="modal-body">
       <div className="mb-3">
   <label htmlFor="exampleFormControlInput1" className="form-label">שם תיק:</label>
-  <input type="text" className="form-control" id="exampleFormControlInput1"/>
+  <input type="text" className="form-control" id="exampleFormControlInput1" onChange={(e: any)=>{setCaseName(e.target.value)} }/>
 </div>
 <div className="mb-3">
   <label htmlFor="exampleFormControlTextarea1" className="form-label">משימות:</label>
-  <textarea className="form-control" id="exampleFormControlTextarea1" rows={4}></textarea>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows={4} onChange={(e: any)=>{setCaseTasks(e.target.value)}}></textarea>
 </div>
 <div className="mb-3">
   <label htmlFor="exampleFormControlInput1" className="form-label">מועד דיון:</label>
@@ -37,7 +59,7 @@ return (
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">בטל</button>
-        <button type="button" className="btn btn-primary">שמור</button>
+        <button type="button" className="btn btn-primary" onClick={saveCase}>שמור</button>
       </div>
     </div>
   </div>
@@ -58,16 +80,9 @@ return (
     <td>{item.caseTasks}</td>
     <td>{item.nextDate}</td>
     <td>  
-      {item.status}
-      <div className="form-check form-switch">
-        {item.status ? 
-        <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked/> :
-        <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault"/> 
+    
+  {item.status ? "פעיל" : "סגור"}
 
-        
-      }
-  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{item.status ? "פעיל" : "סגור"}</label>
-</div>
     </td>
   </tr>
 
